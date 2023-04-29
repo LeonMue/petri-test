961bc9c5d3d91128701549acc4dcb0dc4fe5e52c
Leon Müller <leon.m111@web.de>
2023-04-29 18:28:18 +0200

    make petri-lib generation not dependent on local environment/paths

--- generate.sh
+++ generate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#java -jar ../petri/petri/target/petri-1.0-SNAPSHOT-jar-with-dependencies.jar --psd-path src/test/resources/test.petri --java-out src/main/java/org/dhbw/ka/ml/petritest/ --java-package "org.dhbw.ka.ml.petritest"
-#java -jar ../petri/petri/target/petri-1.0-SNAPSHOT-jar-with-dependencies.jar --psd-path src/test/resources/older_newer_version_extension.petri --java-out src/main/java/org/dhbw/ka/ml/petritest/ --java-package "org.dhbw.ka.ml.petritest"
-export PETRI_PATH=../petri/petri/target/petri-1.0-SNAPSHOT-jar-with-dependencies.jar
+git clone git@github.com:LeonMue/petri.git || git pull
+cd petri/petri && mvn clean package assembly:single
+cd ../../
+export PETRI_PATH=petri/petri/target/petri-1.0-SNAPSHOT-jar-with-dependencies.jar
 for f in $(find ./src/main/java/org/dhbw/ka/ml/petritest/ -name 'generate.sh'); do ./$f; done
