d28ae8f2397fe4aefe567c740e105ad5ce575286
Leon Müller <leon.m111@web.de>
2023-04-29 18:48:33 +0200

    use the new provided installation script

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone git@github.com:LeonMue/petri.git || git pull
-cd petri/petri && mvn clean package assembly:single
+cd petri/petri && ./install.sh
 cd ../../
-export PETRI_PATH=petri/petri/target/petri-1.0-SNAPSHOT-jar-with-dependencies.jar
+export PETRI_PATH=petri/petri/bin/petric.jar
 for f in $(find ./src/main/java/org/dhbw/ka/ml/petritest/ -name 'generate.sh'); do ./$f; done
